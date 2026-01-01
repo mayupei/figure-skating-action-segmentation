16db9bfe79288232e863083de3cf336a804c66cc
mayupei <mayupei39@gmail.com>
2026-01-01 17:36:58 -0500

    auto create models and logs folders

--- scripts/train.py
+++ scripts/train.py
@@ -1,20 +1,23 @@
-import os
 import argparse
+import os
 
 import numpy as np
-from tensorflow.keras.callbacks import CSVLogger
-
 from file_paths import BASE_PATH, DATA_PATH
+from tensorflow.keras.callbacks import CSVLogger
 from utils.config import get_config
 from utils.data_prep import stacking_and_split, stage2_data_by_file
 from utils.models import build_lstm_classifier, cnn_stage2
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description="Train the models on the training set of a split.")
-    parser.add_argument('--split', type=int, required=True,
-                        help='an integer starting from 0')
+    parser = argparse.ArgumentParser(
+        description="Train the models on the training set of a split."
+    )
+    parser.add_argument(
+        "--split", type=int, required=True, help="an integer starting from 0"
+    )
     return parser.parse_args()
+
 
 def main(split):
     config = get_config()
@@ -33,7 +36,7 @@ def main(split):
         hidden_size=config["model_stage1"]["hidden_size"],
         num_classes=config["data_prep"]["class_num"],
     )
-    
+
     stage1_log = CSVLogger(f"logs/stage1_training_{split}.csv", append=False)
     model.fit(
         X,
@@ -41,7 +44,7 @@ def main(split):
         epochs=config["model_stage1"]["epochs"],
         batch_size=config["model_stage1"]["batch_size"],
         validation_split=config["model_stage1"]["validation_split"],
-        callbacks=[stage1_log]
+        callbacks=[stage1_log],
     )
     model.save(os.path.join(BASE_PATH, "models", f"lstm_stage1_{split}.keras"))
 
@@ -62,7 +65,7 @@ def main(split):
         embedding_dim=config["model_stage2"]["embedding_dim"],
         sequence_length=config["model_stage2"]["max_sequence_len_stage2"],
     )
-    
+
     stage2_log = CSVLogger(f"logs/stage2_training_{split}.csv", append=False)
     model2.fit(
         X2,
@@ -70,13 +73,14 @@ def main(split):
         batch_size=config["model_stage2"]["batch_size"],
         epochs=config["model_stage2"]["epochs"],
         validation_split=config["model_stage2"]["validation_split"],
-        callbacks=[stage2_log]
+        callbacks=[stage2_log],
     )
     model2.save(os.path.join(BASE_PATH, "models", f"cnn_stage2_{split}.keras"))
 
 
 if __name__ == "__main__":
     args = parse_args()
-    
-    os.makedirs("logs", exist_ok=True)
+
+    os.makedirs("../logs", exist_ok=True)
+    os.makedirs("../models", exist_ok=True)
     main(args.split)
